4f8c52ef389daff3942b5345b0a8302538810cfe
sngwebs786 <sngwebs786@gmail.com>
2021-09-17 20:37:31 +0500

    Work Done

---
+++ app.js
@@ -0,0 +1,58 @@
+// // question 1 
+
+// function des() {
+//     var arr = ["ali", "sana", "faiza", "hafsa"]
+//     return arr;
+// }
+
+
+// const arr2 = des()
+// let [name1,name2,name3,name4] = arr2;
+// alert(name1)
+// alert(name2)
+// alert(name3)
+// alert(name4)
+
+//=====================================================================
+
+// question 2
+
+// const car = {
+    //     model: "Mehran",
+    //     year: "2011",
+    //     color: "red"
+    // }
+    
+    // let {model,year,color}=car;
+    // alert(model)
+    // alert(year)
+    // alert(color)
+    
+//=====================================================================
+
+//question3
+
+// let name= "Ayesha";
+// let greet = "Hello"
+// let symbol =" ! "
+
+// alert(`${greet} ${symbol}
+// ${name}`)
+
+//=====================================================================
+
+//question4
+
+// const arr=[1,2,3,4,5,6,7,8,9,10]
+// let [var1,var2,var3,...rest]=arr
+// alert(rest)
+
+
+//question5
+
+// let carSpeed = +prompt("Enter the speed of car")
+// let check = (carSpeed<80) ? "car speed is slow" :
+// (carSpeed>80 && carSpeed<120) ? "car sped is moderate":
+// (carSpeed>120)?"car speed is fast": "Error"
+// alert(check)
+
